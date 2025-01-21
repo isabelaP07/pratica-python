5dbe352a111cac5e6135780f67b1c8f670bf512c
Isabela P Felix <isabelaleite07@hotmail.com>
2025-01-21 14:50:48 -0300

    pratica-python

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -6,9 +6,13 @@ total_de_tentativas = 3
 
 for rodada in range (1,total_de_tentativas +1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute_str = input("Digite o seu número: ")
+    chute_str = input("Digite um numero entre 1 e 100: ")
     print("Você digitou: ", chute_str)
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("Voce deve digitar um número entre 1 e 100")
+        continue
 
     acertou = numero_secreto == chute
     maior = chute > numero_secreto
@@ -16,6 +20,7 @@ for rodada in range (1,total_de_tentativas +1):
 
     if (acertou):
         print("Você acertou!")
+        break
     else:
         if (maior):
             print("Você errou! O seu chute foi maior que o número secreto.")
