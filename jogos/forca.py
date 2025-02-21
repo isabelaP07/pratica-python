777f76555bed537bb5feedd17a9acfa7a04c5c63
Isabela P Felix <isabelaleite07@hotmail.com>
2025-02-21 18:23:44 -0300

    jogos

---
+++ jogos/forca.py
@@ -0,0 +1,11 @@
+# forca.py
+
+def jogar():
+
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    print("Fim do jogo")
+
+
